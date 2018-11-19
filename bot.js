6da1fcd5bb4bb20feb1351505c885606bbab796b
JnabY <43906195+jnabyiq@users.noreply.github.com>
2018-11-19 00:46:09 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "-"// البرفكس هنا
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Wenteed')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+
+  
+  
+
+
+});
+ 
+ client.on('ready',async () => {
+console.log("Starting..");
+let g = client.guilds.get("507500031994101763");
+let c = g.channels.get("507859427995418646");
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(!g.me.voiceChannel) c.join();
+}, 1);
+} else {
+console.log("Failed To Join:\n The Channel Type isn't \"text\"");
+}
+});
+ 
+ 
+client.login(process.env.BOT_TOKEN);
